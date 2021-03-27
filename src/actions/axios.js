809a02a6b0ef028f582a779afa2b66418a1b9bf9
Donal Mongey <donalmongey@gmail.com>
2021-03-27 18:26:41 +0000

    Changed baseurl for heroku

--- src/actions/axios.js
+++ src/actions/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
 
-  // baseURL: "https://myblockbot-api.herokuapp.com"
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "https://myblockbot-api.herokuapp.com"
+  // baseURL: "http://127.0.0.1:8000"
 });
 
 export default instance;
