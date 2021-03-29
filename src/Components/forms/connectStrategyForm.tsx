dc6fb8afa82a6369f5f284d49602b1713b6b9566
Donal Mongey <donalmongey@gmail.com>
2021-03-29 17:58:08 +0100

    Fixed issues with stratgy state on form

--- src/Components/forms/connectStrategyForm.tsx
+++ src/Components/forms/connectStrategyForm.tsx
@@ -100,7 +100,7 @@ const ConnectStrategyForm = (props: any) => {
 
     const handleOnChange = (e: any) => {
       
-      setConnectedStrategyState({ ...connectedStrategyState, current_currency: e.target.value, current_currency_balance: 0})
+      setConnectedStrategyState({ ...connectedStrategyState, current_currency: e.target.value, current_currency_balance: 0, initial_first_symbol_balance: 0, initial_second_symbol_balance: 0})
       
     }
 
@@ -174,9 +174,9 @@ const ConnectStrategyForm = (props: any) => {
               <label htmlFor="price" className="block text-sm font-medium text-gray-700">Amount</label>
               <div className="mt-1 relative rounded-md shadow-sm">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <span className="text-gray-500 sm:text-sm">
+                  {/* <span className="text-gray-500 sm:text-sm">
                     $
-                  </span>
+                  </span> */}
                 </div>
                 <input
                   onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -195,8 +195,8 @@ const ConnectStrategyForm = (props: any) => {
                 <div className="absolute inset-y-0 right-0 flex items-center">
                   <label htmlFor="current_currency" className="sr-only">Currency</label>
                   <select onChange={handleOnChange} id="current_currency" name="current_currency" className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
-                    <option>{connectedStrategyState.ticker_1}</option>
-                    <option>{connectedStrategyState.ticker_2}</option>
+                    <option selected={connectedStrategyState.current_currency == connectedStrategyState.ticker_1}>{connectedStrategyState.ticker_1}</option>
+                    <option selected={connectedStrategyState.current_currency == connectedStrategyState.ticker_2}>{connectedStrategyState.ticker_2}</option>
                   </select>
                 </div>
               </div>
